3dd3a2dec3af488e0cea18b527f7b2e9dd4f43f3
fikeretebogale <mishtekele@gmail.com>
2021-03-10 22:25:56 -0600

    Update script.js
    new change

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ let myVideoStream;
 navigator.mediaDevices
 	.getUserMedia({
 		video: true,
-		audio: false,
+		audio: true,
 	})
 	.then((stream) => {
 		myVideoStream = stream;
@@ -71,3 +71,66 @@ function connectToNewUser(userId, stream) {
 
 	peers[userId] = call;
 }
+
+// function addVideoStream(video, stream) {
+// 	video.srcObject = stream;
+// 	video.addEventListener("loadedmetadata", () => {
+// 		video.play();
+// 	});
+// 	videoGrid.append(video);
+// }
+
+const playStop = () => {
+	console.log("object");
+	let enabled = myVideoStream.getVideoTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getVideoTracks()[0].enabled = false;
+		setPlayVideo();
+	} else {
+		setStopVideo();
+		myVideoStream.getVideoTracks()[0].enabled = true;
+	}
+};
+
+const muteUnmute = () => {
+	const enabled = myVideoStream.getAudioTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false;
+		setUnmuteButton();
+	} else {
+		setMuteButton();
+		myVideoStream.getAudioTracks()[0].enabled = true;
+	}
+};
+
+const setStopVideo = () => {
+	const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+	document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+	const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+	document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setMuteButton = () => {
+	const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+	document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+	const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+	document.querySelector(".main__mute_button").innerHTML = html;
+};
